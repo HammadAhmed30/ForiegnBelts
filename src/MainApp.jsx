feff7f0a83dd75b053a0ec128930fb2a005acab0
mahmadali3180 <mahmadali3180@gmail.com>
2023-06-19 16:35:35 +0500

    update with main

--- src/MainApp.jsx
+++ src/MainApp.jsx
@@ -3,14 +3,16 @@ import HomePage from "./components/Pages/HomePage";
 import TestPageForInventoryPage from "./components/Pages/TestPageForInventoryPage";
 import TestPageForLikedInventory from "./components/Pages/TestPageForLikedInventory";
 import TestPageForProductDetails from "./components/Pages/TestPageForProductDetails";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 export default function MainApp() {
   return (
     <div>
-      {/* <HomePage /> */}
-      {/* <TestPageForInventoryPage /> */}
-      {/* <TestPageForLikedInventory /> */}
-      <TestPageForProductDetails />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/inventory" element={<TestPageForInventoryPage />} />
+        <Route path="/liked" element={<TestPageForLikedInventory />} />
+      </Routes>
     </div>
   );
 }
