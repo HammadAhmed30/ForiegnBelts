fc2418f117940e4d79db987b6e2f3ca132fe6e7d
Hammad Ahmed <30lazers@gmail.com>
2023-06-19 12:37:44 +0500

    Inventory Like Page responsive done

--- src/components/Pages/TestPageForLikedInventory.jsx
+++ src/components/Pages/TestPageForLikedInventory.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "../../styles/TestPageForLikedInventory.css";
+import "../../styles/TestPageForInventoryPage.css";
 import LikedInventory from "../components/InventoryPageByAhmad/LikedInventory";
-import SendAndCall from "../components/InventoryPageByAhmad/SendAndCall";
 
 function TestPageForLikedInventory() {
   return (
     <div className="width-100">
       <LikedInventory />
-      <SendAndCall />
     </div>
   );
 }
